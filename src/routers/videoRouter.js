dc64858082c741c28380fc7bfa5604011e196db9
LooanCheong <jjyejun@naver.com>
2022-12-31 07:44:32 +0900

    make a edit page

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
 import {
   watch,
-  edit,
+  getEdit,
   upload,
   deleteVideo,
+  postEdit,
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
