b593fdfe34423d9c62f463af1d956b18a8a6b081
LooanCheong <jjyejun@naver.com>
2023-02-09 21:56:30 +0900

    비디오 녹화 및 다운로드 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,54 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+
+  video.pause();
+  video.src = "";
+  stream.getTracks()[0].stop();
 };
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (e) => {
+    videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: { width: 200, height: 100 },
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
 
 //이벤트 리스너
 startBtn.addEventListener("click", handleStart);
