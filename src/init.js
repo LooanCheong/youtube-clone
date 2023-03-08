c847fedcb2b8e9da998227d52743bdad25dd6387
LooanCheong <jjyejun@naver.com>
2023-03-08 22:31:54 +0900

    포트 환경 설정 변경

--- src/init.js
+++ src/init.js
@@ -6,9 +6,9 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 8080;
 
 const handleListening = () =>
   console.log(`✅ server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, "0.0.0.0", handleListening);
