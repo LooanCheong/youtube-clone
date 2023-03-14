20aec0a6956cd1d66cc5beddf0e1a19618775324
LooanCheong <jjyejun@naver.com>
2023-03-15 04:24:56 +0900

    multer 업로드 경로 수정

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,15 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "plam",
+  bucket: "plam/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "plam/videos",
   acl: "public-read",
   contentType: multerS3.AUTO_CONTENT_TYPE,
 });
@@ -44,11 +50,11 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars",
   limits: { fileSize: 3000000 },
-  storage: multerUploader,
+  storage: s3ImageUploader,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos",
   limits: { fileSize: 10000000 },
-  storage: multerUploader,
+  storage: s3VideoUploader,
 });
